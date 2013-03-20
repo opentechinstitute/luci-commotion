37a903dc1b205681362151bf353ec2e053a168ed
Andrew Reynolds <andrew@opentechinstitute.org>
2013-03-20 10:03:06 -0400

    Rudimentary form action in place

--- luasrc/controller/commotion/meshprofile.lua
+++ luasrc/controller/commotion/meshprofile.lua
@@ -23,18 +23,11 @@ function index()
 	require("luci.i18n").loadc("commotion")
 	local i18n = luci.i18n.translate
 
+	entry({"admin", "commotion", "meshprofile_submit"}, call("ifprocess"))
 	entry({"admin", "commotion", "meshprofile"}, call("main"), "Mesh Profile (New)", 20).dependent=false
 end
 
 function main()
-	-- if return values get them and pass them to return value parser --
-	--[[ Ignoring upload until luci-ssl is enabled
-	setFileHandler()
-	check = luci.http.formvalue()
-	if next(check) ~= nil then
-	   errorMsg = checkPage()
-	end 
-		]]--	
 	local rawProfiles = luci.fs.dir(profileDir)	
 	local profiles = {}
 	for i,p in ipairs(rawProfiles) do
@@ -49,79 +42,17 @@ function main()
 	luci.template.render("commotion/meshprofile", {profiles = profiles})
 end
 
+function ifprocess()
+	log("PROCESSING MEAT")
+	finish()
+end
+
 function checkPage()
    local returns = luci.http.formvalue()
    errors = parseSubmit(returns)
    return errors
 end
---[[ Build without upload until luci-ssl is enabled 
-function setFileHandler()                                                                                                                                                                                    
-	local sys = require "luci.sys"                                                                                                                                                                            
-	local fs = require "luci.fs"                                                                                                                                                                              
-        -- causes media files to be uploaded to their namesake in the /tmp/ dir.                                                                                                                                  
-        local fp                                                                                                                                                                                                  
-        luci.http.setfilehandler(
-        -- Use profileDir .. meta.file instead of "quickstartMesh" --
-	function(meta, chunk, eof)                                                                                                                                                                         
-		if not fp then                                                                                                                                                                              
-        		if meta and meta.name == "config" then                                                                                                                                               
-        			fp = io.open(profileDir .. meta.file, "w")                                                                                                                                  
-        		end
-        		if chunk then                                                                                                                                                                        
-        			fp:write(chunk)                                                                                                                                                                   
-        		end                                                                                                                                                                                  
-        		if eof then                                                                                                                                                              
-        			fp:close()                                                                                                                                                            
-        		end                                                                                                                                                                      
-        	end                                                                                                                                                                             
-	end)                                                                                                                                                                                   
-end
 
-
-function uploadRenderer()                                                                                                                      
---creates an uploader based upon the fileType of the page config
-	local uci = luci.model.uci.cursor()
-	local page = uci:get('quickstart', 'options', 'pageNo')
-        local fileType = uci:get('quickstart', page, 'fileType')
-        --TODO check uploader module to see if it needs any values
-        if fileType == 'config' then
-        	fileInstructions="and submit a config file from your own computer. You will be able to customize this configuration once it has been applied to the node."
-        elseif fileType == 'key' then
-        	fileInstructions="and submit a key file from your own computer. This will allow your node to talk to any network with the same key file"           
-        end
-        return {['fileType']=fileType, ['fileInstructions']=fileInstructions}
-end
-                                                                                                                                                                                                     
-function uploadParser()                                                                                                                                      
---Parses uploaded data                                                                                                                                    
-	local uci = luci.model.uci.cursor()                                                                                                                       
-        error = ''                                                                                                                                             
-        if luci.http.formvalue("config") ~= '' then                                                                                                               
-        	file = luci.http.formvalue("config")                                                                                                               
-        elseif luci.http.formvalue("config") == '' then                                                                                                           
-        	error = "Please upload a setting file."                                                                                                            
-        elseif luci.http.formvalue("key") ~= '' then                                                                                                              
-        	file = luci.http.formvalue("key")                                                                                                                  
-        end
-        if file then
-        	if luci.http.formvalue("config") then
-        		--check that each file is actually the file type that we are looking for!!!
-        		if not uci:get('nodeConf', 'confInfo', 'name') then
-        			error = 'This file is not a configuration file. Please check the file and upload a working config file or go back and choose a pre-built config'
-        		end
-        	elseif luci.http.formvalue("key") then
-        		if luci.sys.call("pwd") == '1' then
-        			elseif luci.sys.call("servald keyring list") == '1' then
-        			error = 'The file uploaded is either not a proper keyring or has a pin that is required to access the key within. If you do not think that your keyring has a pin please upload a proper servald keyring for your network key. If your keyring is pin protected, please click continue below.'
-        		end
-        	end
-        end
-        if error ~= '' then
-        	return error
-        end
-end
-
-]]--	
 function finish()
    luci.sys.call("/etc/init.d/commotiond restart")
    luci.sys.call("sleep 2; /etc/init.d/network restart")
