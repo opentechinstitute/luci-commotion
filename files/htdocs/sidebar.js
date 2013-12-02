9b95c1731f6f1f7dc337a9efb8d943e4b3a9346b
Griffin Boyce <griffinboyce@gmail.com>
2013-12-02 15:09:12 -0500

    intial sidebar

---
+++ files/htdocs/sidebar.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  var animTime = 250,
+      clickPolice = false;
+  
+  $(document).on('touchstart click', '.acc-btn', function(){
+    if(!clickPolice){
+       clickPolice = true;
+      
+      var currIndex = $(this).index('.acc-btn'),
+          targetHeight = $('.acc-content-inner').eq(currIndex).outerHeight();
+   
+      $('.acc-btn').removeClass('selected');
+      $(this).addClass('selected');
+      
+      $('.acc-content').stop().animate({ height: 0 }, animTime);
+      $('.acc-content').eq(currIndex).stop().animate({ height: targetHeight }, animTime);
+
+      setTimeout(function(){ clickPolice = false; }, animTime);
+    }
+    
+  });
+  
+});
