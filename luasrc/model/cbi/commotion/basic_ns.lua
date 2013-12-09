46d84aa3f9ea7f18d5330d95e8f43f5915364e62
Seamus Tuohy <s2e@opentechinstitute.org>
2013-12-09 15:23:00 -0500

    moved the initial creation of uci objects into a uci-defaults script where it rightfully should go. Why run the logic everytime when we can do it only once.

--- luasrc/model/cbi/commotion/basic_ns.lua
+++ luasrc/model/cbi/commotion/basic_ns.lua
@@ -6,15 +6,6 @@ local uci = require "luci.model.uci".cursor()
 --Main title and system config map for hostname value
 local m = Map("system", translate("Basic Configuration"), translate("In this section you'll set the basic required settings for this device, and the basic network settings required to connect this device to a Commotion Mesh network. You will be prompted to save your settings along the way and apply them at the end."))
 
--- Add Wireless Interfaces for rest of quickstart process
-function m.on_parse() 
-   if QS.status() then
-	  uci:section("wireless", "wifi-iface", "quickstartAP", {mode="ap", network="client"})
-	  uci:section("wireless", "wifi-iface", "quickstartMesh", {mode="adhoc"})
-	  uci:save("wireless")
-	  uci:commit("wireless")
-   end
-end
 
 --HOSTNAME
 
