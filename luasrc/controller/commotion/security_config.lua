1e4bea7d564d8de194fac12a24176e2eb02bea68
Seamus Tuohy <s2e@opentechinstitute.org>
2013-12-10 14:40:33 -0500

    updated security config to work with mesh keyring

--- luasrc/controller/commotion/security_config.lua
+++ luasrc/controller/commotion/security_config.lua
@@ -1,17 +1,20 @@
 --[[
-LuCI - Lua Development Framework
-
-Copyright 2013 - Seamus Tuohy <s2e@opentechinstitute.org>
-
-With Thanks to the niu suite built by Steven Barth <steven@midlink.org>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-	http://www.apache.org/licenses/LICENSE-2.0
-
+   Copyright (C) 2013 Seamus Tuohy 
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program. If not, see <http://www.gnu.org/licenses/>.
 ]]--
+
 module "luci.controller.commotion.security_config"
 
 function index()
@@ -21,3 +24,4 @@ function index()
 
 	  entry({"admin", "commotion", "security", "shared_mesh_keychain"}, cbi("commotion/security_smk", {hideapplybtn=true, hideresetbtn=true}), translate("Shared Mesh Keychain"), 60).subsection=true
 end
+
