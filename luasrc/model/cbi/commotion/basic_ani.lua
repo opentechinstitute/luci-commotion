253d4fe017fe958f101a5b295e25cf3e68bdee1e
Seamus Tuohy <s2e@opentechinstitute.org>
2013-12-11 19:26:08 -0500

    corrected spelling of SETUP wizard

--- luasrc/model/cbi/commotion/basic_ani.lua
+++ luasrc/model/cbi/commotion/basic_ani.lua
@@ -19,7 +19,7 @@ local utils = require "luci.util"
 local cnw = require "luci.commotion.network"
 local db = require "luci.commotion.debugger"
 local http = require "luci.http"
-local SW = require "luci.commotion.startup_wizard"
+local SW = require "luci.commotion.setup_wizard"
 local cdisp = require "luci.commotion.dispatch"
 
 m = Map("network", translate("Internet Gateway"), translate("If desired, you can configure your gateway interface  here."))
