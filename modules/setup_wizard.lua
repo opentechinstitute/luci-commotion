8ef5ad5ef5860b02079cf1098b716fed313269b9
Seamus Tuohy <s2e@opentechinstitute.org>
2013-12-12 15:04:00 -0500

    added functional setup wizard script

--- modules/setup_wizard.lua
+++ modules/setup_wizard.lua
@@ -7,8 +7,12 @@ local SW = {}
 --! @return true if on, false if completed
 --! @TODO IMPLEMENT THIS: Currently placeholder
 function SW.status()
-   return false
-   --return true
+   local uci = require "luci.model.cui".cursor()
+   if uci:get("setup_wizard", "settings", "enabled") == "1" then
+	  return true
+   else
+	  return false
+   end
 end
 
 return SW
