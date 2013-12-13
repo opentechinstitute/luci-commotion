4cf6f74584576712e15e3d05b4d92f0531994194
Seamus Tuohy <s2e@opentechinstitute.org>
2013-12-13 14:52:50 -0500

    updated the pseudo-code while we wait for commotiond to get bindings

--- modules/network.lua
+++ modules/network.lua
@@ -3,7 +3,7 @@
 local utils = require "luci.util"
 local sys = require "luci.sys"
 local uci = require "luci.model.uci"
-local string, table, tostring = string, table, tostring
+local string, table, tostring, pairs = string, table, tostring, pairs
 
 module "luci.commotion.network"
 
@@ -60,27 +60,120 @@ function network.get_channels(mode, short)
    end
 end
 
+--! @name commotiond
+--! @brief a local function which currently calls the commotiond command line function. This will hopfully be replaces with lua/c bindings later.
+--! param cmd string The command to append to commotiond
+--! param err bool true if you want to receive the error code nil if you wan the standard output
+--! @return the standard out of the commotiond call
+local function network.commotiond(cmd, err)
+   if err then
+	  return sys.call("commotiond "..cmd)
+   else
+	  return sys.exec("commotiond "..cmd)
+   end
+end
+
+
 --! @name commotion_set
 --! @brief finds, or creates a commotion profile and sets values
 --! param name string name of profile
 --! param options table options in key/value pairs {option="value", option2="value2"}
---! @return boolean value stating success or failure
+--! @return boolean value stating success or failure and errors if any on failure
 function network.commotion_set(name, options)
-   --if not find_profile(name) then
-   --error = create_profile(name)
-   --if error then return false end
-   --end
-   --for oname,oval in pairs(options) do
-   --error = set_option(name, oname, oval)
-   --if error == "error" then
-   --revert_changes(name)
-   --return false
-   --end
-   --end
+   return true --! TODO ALERT this renders this function useless until commotiond functionality is enabled.
+   local function setop(opts, err, pr)
+	  for i,x in pairs(opts) do
+		 if not network.commotiond("set "..pr.." "..i.." "..x, true) then
+			table.insert(err, {i,x})
+		 end
+	  end
+	  return err
+   end
+   local profiles = network.commotiond("profiles", true)
+   local profile = nil
+   local errors = {}
+   for _,prof in ipairs(prof) do
+	  if prof == name then
+		 profile = name
+	  end
+   end
+   if profile then
+	  errors = setop(options, errors, profile)
+   else
+	  local create = network.commotiond(name, true)
+	  if create then
+		 errors = setop(options, errors, name)
+	  else
+		 table.insert(errors, {"profile", "created"}
+	  end
+   end
+   if errors then
+	  return nil, errors
+   else
+	  return true
+   end
    return true
 end
 
+--! @name nodeid
+--! @brief finds, or creates the nodeid 
+--! param name string name of profile
+--! param options table options in key/value pairs {option="value", option2="value2"}
+--! @return boolean value stating success or failure and errors if any on failure
+function network.nodeid(new_id)
+   return "abcdefghijklmnopqrstuvwxyznowiknowmyabcs"
+   --! TODO ALERT this renders this function useless until commotiond functionality is enabled.
+   if new_id then
+	  return network.commotiond("nodeid "..new_id, true)
+   else
+	  return network.commotiond("nodeid")
+   end	  
+end
 
+--[[
+
+Commotiond commands to add functionality for
+   
+up <iface> <profile> -same as before
+down <iface> -same as before
+status <iface> -same as before
+state <iface> <property> -same as before
+profiles -replaces list_profiles
+set <profile> <property> <value> -allows you to set profile values
+get <profile> <property> <value> -allows you to get profile values
+save <profile> <file> -allows you to save a profile to a new file in the
+profiles.d directory
+new <profile> -allows you to create a new profile
+delete <profile> -allows you to delete a profile
+ipgen <ip address> <netmask> -allows you to generate an arbitrary IP
+address from the nodeid
+nodeid <id> -run without arguments, prints the nodeid. Can also set the
+nodeid.
+]]--
+
+--[[
+   And here's the new config file:
+{
+  "ssid": "commotionwireless.net",
+  "bssid": "02:CA:FF:EE:BA:BE",
+  "bssidgen": "true"
+  "channel": "5",
+  "type": "mesh",
+  "dns": "8.8.8.8",
+  "domain": "mesh.local",
+  "ipgen": "true",
+  "ipgenmask": "255.0.0.0",
+  "mode": "adhoc",
+  "ip": 100.64.0.0,
+  "netmask": "255.192.0.0",
+  "wpa": "true",
+  "wpakey": "c0MM0t10n!r0cks",
+  "servald": "false",
+  "servalsid": "",
+  "announce": "true"
+}
+
+]]--
 
 
 return network
