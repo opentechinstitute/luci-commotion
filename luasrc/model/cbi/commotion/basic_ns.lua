9e6da7b6932d879ff151d992a3c999f7d25d162e
Seamus Tuohy <s2e@opentechinstitute.org>
2013-12-13 14:30:41 -0500

    removed unneeded comment

--- luasrc/model/cbi/commotion/basic_ns.lua
+++ luasrc/model/cbi/commotion/basic_ns.lua
@@ -26,9 +26,6 @@ local m = Map("system", translate("Basic Configuration"), translate("In this sec
 --redirect on saved and changed to check changes.
 if not SW.status() then
    m.on_after_save = cdisp.conf_page
-else
-   db.log("TODO add option to end quickstart!")
-   --! TODO have network settings section add in a option to end quickstart that will be set in the last step on the confirmation page. Or possibly do this in the last step in case they exit out after this? But, it wont save either way I don't think. Test on restart after this step to see if it does. 
 end
 
 --load up system section
