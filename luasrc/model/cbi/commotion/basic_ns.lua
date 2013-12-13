7e8af2eaedcba92eaa213b4d3e513c340c4c034c
Seamus Tuohy <s2e@opentechinstitute.org>
2013-12-13 18:19:39 -0500

    added a correct admin password check. Still need to do changed password comparisons.

--- luasrc/model/cbi/commotion/basic_ns.lua
+++ luasrc/model/cbi/commotion/basic_ns.lua
@@ -20,7 +20,7 @@ local db = require "luci.commotion.debugger"
 local uci = require "luci.model.uci".cursor()
 local cdisp = require "luci.commotion.dispatch"
 local cnet = require "luci.commotion.network"
-
+local http = require "luci.http"
 --Main title and system config map for hostname value
 local m = Map("system", translate("Basic Configuration"), translate("In this section you'll set the basic required settings for this device, and the basic network settings required to connect this device to a Commotion Mesh network. You will be prompted to save your settings along the way and apply them at the end."))
 --redirect on saved and changed to check changes.
@@ -48,22 +48,19 @@ end
 
 
 --PASSWORDS
-
 local v0 = true -- track password success across maps
 
+-- CURRENT PASSWORD
 -- Allow incorrect root password to prevent settings change
 -- Don't prompt for password if none has been set
 if luci.sys.user.getpasswd("root") then
-   s0 = m:section(TypedSection, "_dummy", translate("Current Password"),
-				  translate("Current password required to make changes on this page"))
+   s0 = m:section(TypedSection, "_dummy", translate("Current Password"), translate("Current password required to make changes on this page"))
    s0.addremove = false
    s0.anonymous = true
-   
-   pw0 = s0:option(Value, "pw0", translate("Current Password"))
+   pw0 = s0:option(Value, "_pw0")
    pw0.password = true
    -- fail by default
    v0 = false
-   
    function s0.cfgsections()
 	  return { "_pass0" }
    end
@@ -89,34 +86,55 @@ function s.cfgsections()
 	return { "_pass" }
 end
 
-function m.on_before_commit(map)
-	-- if existing password, make sure user has old password
-	if s0 then
-		v0 = luci.sys.user.checkpasswd("root", formvalue("_pass0"))
-	end
-
-	if v0 == false then
-		m.message = translate("Incorrect password. Changes rejected!")
-		m.save=v0
-		m2.save=v0
-	end
+function m.on_parse(map)
+   local form = http.formvaluetable("cbid")
+   local check = nil
+   local conf_pass = nil
+   for field,val in pairs(form) do
+	  string.gsub(field, ".-_pw(%d)$",
+				  function(num)
+					 if tonumber(num) == 0 then
+						conf_pass = val
+					 end
+					 if val then
+						check = true
+					 end
+				  end)
+   end
+   if check ~= nil then
+	  if conf_pass then
+		 v0 = luci.sys.user.checkpasswd("root", conf_pass)
+		 if v0 ~= true then
+			m.message = translate("Incorrect password. Changes rejected!")
+			m.save = false
+			function m.on_after_save() return true end --Don't redirect on error
+		 end
+	  else
+		 m.message = translate("Please enter your old password. Changes rejected!")
+		 m.save = false
+	  end
+   end
 end
 
-function m.on_commit(map)
-   local v1 = pw1:formvalue("_pass")
-   local v2 = pw2:formvalue("_pass")
+function m.on_save(map) --! @TODO this does not check any of the pasword checks with the new form.
+   local v1 = pw1:formvalue("_pw1")
+   local v2 = pw2:formvalue("_pw2")
 	
 	if v0 == true and v1 and v2 and #v1 > 0 and #v2 > 0 then
 	   if v1 == v2 then
 		  if luci.sys.user.setpasswd(luci.dispatcher.context.authuser, v1) == 0 then
 			 m.message = translate("Password successfully changed!")
-			 uci:set("setup_wizard", "tracking", "adminPass", 'true')
-			 uci:save("setup_wizard")
-			 uci:commit("setup_wizard")
+			 if SW.status() then
+				uci:set("setup_wizard", "tracking", "adminPass", 'true')
+				uci:save("setup_wizard")
+				uci:commit("setup_wizard")
+			 end
 		  else
+			 --function m.on_after_save() return true end
 			 m.message = translate("Unknown Error, password not changed!")
 		  end
 	   else
+		  --function m.on_after_save() return true end
 		  m.message = translate("Given password confirmation did not match, password not changed!")
 	   end
 	end
